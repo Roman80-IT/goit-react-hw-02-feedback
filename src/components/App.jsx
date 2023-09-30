e5c8285ce77b4a46f2cad2e23ee6b73269f8f8f7
Roman <jrj80mail@gmail.com>
2023-09-30 21:04:13 +0300

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // Метод збільшення відгуків (одним методом)
+  incrementFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="text">Please leave feedback</h1>
+        <ul>
+          <li>
+            <button onClick={() => this.incrementFeedback('good')}>Good</button>
+          </li>
+          <li>
+            <button onClick={() => this.incrementFeedback('neutral')}>
+              Neutral
+            </button>
+          </li>
+          <li>
+            <button onClick={() => this.incrementFeedback('bad')}>Bad</button>
+          </li>
+        </ul>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+      </>
+    );
+  }
+}
