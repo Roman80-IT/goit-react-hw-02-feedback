15219a28f533793e94899ff4612a6e25ae0eb516
Roman <jrj80mail@gmail.com>
2023-10-01 19:01:06 +0300

    step 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -9,56 +10,6 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-
-  //   incrementFeedback = option => {
-  //     this.setState(prevState => ({
-  //       [option]: prevState[option] + 1,
-  //     }));
-  //   };
-
-  //   countTotalFeedback = () => {
-  //     const { good, neutral, bad } = this.state;
-  //     return good + neutral + bad;
-  //   };
-
-  //   countPositiveFeedbackPercentage = () => {
-  //     const { good } = this.state;
-  //     const total = this.countTotalFeedback();
-  //     return total === 0 ? 0 : (good / total) * 100;
-  //   };
-
-  //   render() {
-  //     const totalFeedback = this.countTotalFeedback();
-  //     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-  //     return (
-  //       <>
-  //         <h1 className="text">Please leave feedback</h1>
-  //         <ul>
-  //           <li>
-  //             <button onClick={() => this.incrementFeedback('good')}>Good</button>
-  //           </li>
-  //           <li>
-  //             <button onClick={() => this.incrementFeedback('neutral')}>
-  //               Neutral
-  //             </button>
-  //           </li>
-  //           <li>
-  //             <button onClick={() => this.incrementFeedback('bad')}>Bad</button>
-  //           </li>
-  //         </ul>
-  //         <h2>Statistics</h2>
-  //         <ul>
-  //           <li>Good: {this.state.good}</li>
-  //           <li>Neutral: {this.state.neutral}</li>
-  //           <li>Bad: {this.state.bad}</li>
-  //           <li>Total feedback: {totalFeedback}</li>
-  //           <li>Positive feedback: {positivePercentage.toFixed(2)}%</li>
-  //         </ul>
-  //       </>
-  //     );
-  //   }
-  // }
 
   // Метод збільшення відгуків (одним методом)
   incrementFeedback = option => {
@@ -94,13 +45,17 @@ export class App extends Component {
         </Section>
 
         <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={positivePercentage}
-          />
+          {totalFeedback === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          )}
         </Section>
       </div>
     );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,7 +7,7 @@ export const Statistics = ({
 }) => {
   return (
     <div>
-      <h2>Statistics</h2>
+      {/* <h2>Statistics</h2> */}
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
